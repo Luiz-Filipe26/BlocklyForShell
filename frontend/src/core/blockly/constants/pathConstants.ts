61bda59bb01ff200983cbe26dee6af675ca881ad
Luiz-Filipe-26 <luizferamos26@gmail.com>
2025-12-14 22:54:16 -0300

    Mudado frontend/public para usar kebab-case

--- frontend/src/core/blockly/constants/pathConstants.ts
+++ frontend/src/core/blockly/constants/pathConstants.ts
@@ -1,5 +1,5 @@
 export const PATH_CONSTANTS = {
     CARD_ICON_EMPTY: "/transparent.svg",
-    CARD_ICON_ALERT: "/cardinality_icon.svg",
-    INFO_ICON: "/info_icon.svg",
+    CARD_ICON_ALERT: "/cardinality-icon.svg",
+    INFO_ICON: "/info-icon.svg",
 } as const;
